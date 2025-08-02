adac09285bedffb38d71f08db6bb85fb43aa9ed5
Eric <eriico39hi@gmail.com>
2025-08-01 21:37:14 -0400

    Last commit was a fail
    Committed wrong folder, didn't have the actual changes, this one has all the changes

---
+++ server/index.js
@@ -0,0 +1,60 @@
+//Includes
+const express = require("express")
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const Users = require("./model/Users")
+const Recipes = require("./model/Recipes")
+const dotenv = require("dotenv")
+const jwt = require("jsonwebtoken")
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+
+//Service post request to register new account
+app.post("/api/auth/register", async (req,res)=>{
+    try{
+        const {username, password} = req.body
+        let user = await Users.findOne({username})
+
+        if(user) return res.status(401).json({"msg":"A user already exists with that username"})
+        const passwordHash = await bcrypt.hash(password, 10)
+        user = new Users({username,password:passwordHash})
+        user.save()
+
+        const token = await jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"1h"})
+        res.status(200).json({id:user._id,token})
+    }
+    catch(err){console.log(err)}
+})
+
+//Service post request to login
+app.post("/api/auth/login", async (req,res)=>{
+
+    
+})
+
+//Service post request to submit new recipe to database
+app.post("/api/recipesubmit", async (req,res)=>{
+
+    
+})
+
+//Service get request for my recipes
+app.get("/api/myrecipes", async (req,res)=>{
+
+
+})
+
+//Create connection to mongoDB and start server
+async function serverStart(){
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        app.listen(process.env.PORT,()=>{
+            console.log('Server has started')
+        })
+    }
+    catch(err){console.log(err)}
+}
+
+serverStart()
---
+++ server/model/Users.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+const usersSchema = mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+})
+
+const Users = mongoose.model('Users',usersSchema)
+module.exports = Users
