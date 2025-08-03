78820250e7c2670ee7fc9c0e62071abb49a1296d
Eric <eriico39hi@gmail.com>
2025-08-03 11:47:23 -0400

    First attempt at recipe submit
    Untested recipe submit back-end attempt

--- server/index.js
+++ server/index.js
@@ -30,14 +30,35 @@ app.post("/api/auth/register", async (req,res)=>{
 
 //Service post request to login
 app.post("/api/auth/login", async (req,res)=>{
+    const {username, password} = req.body
 
-    
+    try{
+        let user = await Users.findOne({username})
+        if(!user) return res.status(401).json({"msg":"Wrong username or password"})
+
+        const passwdTest = await bcrypt.compare(password,user.password)
+        if (!passwdTest) return res.status(401).json({"msg":"Wrong username or password"})
+        
+        const token = await jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"1h"})
+        res.status(200).json({id:user._id,token})
+    }
+    catch(err){console.log(err)}    
 })
 
 //Service post request to submit new recipe to database
 app.post("/api/recipesubmit", async (req,res)=>{
-
+    const {name, time, description, ingredients, instructions} = req.body
     
+    try{
+        
+        let recipe = await Recipes.findOne({name})
+
+        if(recipe) return res.status(401).json({"msg":"A recipe with that name already exists"})
+        
+        recipe = new Recipes({name, time, description, ingredients, instructions})
+        recipe.save()
+    }
+    catch(err){console.log(err)}    
 })
 
 //Service get request for my recipes
---
+++ server/model/Recipes.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+const recipeSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    time:{
+        type:Number,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    ingredients:{
+        type:Array,
+        required:true
+    },
+    instructions:{
+        type:Array,
+        required:true
+    },
+})
+
+const Recipes = mongoose.model('Recipes',recipeSchema)
+module.exports = Recipes
