72c9c781b50e3ff15d39e1aeb87b6f306d8910fe
Eric <eriico39hi@gmail.com>
2025-08-05 12:01:53 -0400

    Worked on create recipe
    worked a bunch on create recipe. Nothing is working yet though. Has file form now.

--- client/src/components/BrowseRecipes.jsx
+++ client/src/components/BrowseRecipes.jsx
@@ -3,11 +3,14 @@ import { useState } from "react";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Card from 'react-bootstrap/Card'
 
 
 function BrowseRecipes() {
    return (<>
         
+    
+
     <Navbar expand="lg" style={{ backgroundColor: '#74cbe0ff' }}>
       <Container>
         <Navbar.Brand className="fs-4 fw-bold" href="/Home">CuisineShare</Navbar.Brand>
@@ -15,6 +18,19 @@ function BrowseRecipes() {
       </Container>
     </Navbar>
         <p> Browse Recipes Under Construction</p>
+
+    <Card style={{ width: '16rem' }}>
+      <Card.Img variant="top" src="/src/assets/Sandwich.jpg" />
+      <Card.Body>
+        <Card.Title>Sandwich</Card.Title>
+        <Card.Text>
+          Delicious Sandwich Recipe
+        </Card.Text>
+      </Card.Body>
+    </Card>
+
+
+
     </>);
     
 }
